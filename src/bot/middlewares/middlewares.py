74421c53f2f2c12c77851e92da1233a62caba622
@PyAdminRU <zh.vitaliy92@gmail.com>
2025-06-25 23:44:39 +0300

    Переход с sqlite на peewee

--- src/bot/middlewares/middlewares.py
+++ src/bot/middlewares/middlewares.py
@@ -4,24 +4,34 @@ from aiogram.types import Message
 
 from src.core.config.config import ADMIN
 from src.core.database.database import db
+from loguru import logger
 
 
 class AdminFilter(Filter):
     """Проверка на права админа"""
 
     async def __call__(self, message: Message) -> bool:
-        return True if message.from_user.id == ADMIN else False
+        try:
+            return True if message.from_user.id == ADMIN else False
+        except Exception as e:
+            logger.exception(e)
 
 
 class ManagerAppealsFilter(Filter):
     """Проверка на наличие активных обращений"""
 
     async def __call__(self, message: Message) -> bool:
-        return await db.check_manager_active_appeal(message.from_user.id)
+        try:
+            return await db.check_manager_active_appeal(message.from_user.id)
+        except Exception as e:
+            logger.exception(e)
 
 
 class UserAppealsFilter(Filter):
     """Проверка на наличие активных обращений"""
 
     async def __call__(self, message: Message) -> bool:
-        return await db.check_user_active_appeal(message.from_user.id)
+        try:
+            return await db.check_user_active_appeal(message.from_user.id)
+        except Exception as e:
+            logger.exception(e)
