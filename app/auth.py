6993bc79a14a71a581c11c8fe9a564c778a991d0
@PyAdminRU <zh.vitaliy92@gmail.com>
2025-06-25 15:47:59 +0300

    Реализация авторизации пользователя.

--- app/auth.py
+++ app/auth.py
@@ -1,9 +1,9 @@
 # auth.py
-from fastapi import APIRouter, Request, Form, Depends, Response
+from typing import Optional
+
+from fastapi import APIRouter, Request, Form
 from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
-
-from typing import Optional
 
 router = APIRouter()
 
